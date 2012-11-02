fa43da74b7aaf69b8da3b140887cc809634cbec4
Ben Trask <bentrask@comcast.net>
2012-11-02 13:54:55 -0400

    Add Array.isArray() implementation for IE8.

--- client/gallery/external/Array.js
+++ client/gallery/external/Array.js
@@ -31,3 +31,10 @@ if (!Array.prototype.indexOf) {
         return -1;
     }
 }
+
+// <https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/isArray>
+if(!Array.isArray) {
+  Array.isArray = function (vArg) {
+    return Object.prototype.toString.call(vArg) === "[object Array]";
+  };
+}
