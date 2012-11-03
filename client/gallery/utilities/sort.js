dd2b42e45cafcb1b68eb23fcf1e888fdb8be9b1e
Ben Trask <bentrask@comcast.net>
2012-11-03 09:26:44 -0400

    Faster numericStringCompare() with regular expressions.

--- client/gallery/utilities/sort.js
+++ client/gallery/utilities/sort.js
@@ -22,24 +22,18 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 var sort;
 try { sort = exports; } catch(e) { sort = {}; }
 
-sort.numericStringCompare = function(a, b) {
-	a = a.toLocaleLowerCase();
-	b = b.toLocaleLowerCase();
-	var ca, cb, diff;
-	function numeric(c) {
-		return c >= "0" && c <= "9";
+sort.numericStringCompare = function(a1, b1) {
+	var ra = /(\D*)(\d*)/g;
+	var rb = /(\D*)(\d*)/g;
+	var a2, b2, r;
+	for(;;) {
+		a2 = ra.exec(a1);
+		b2 = rb.exec(b1);
+		if(!a2 && !b2) return 0;
+		if(!a2) return -1;
+		if(!b2) return 1;
+		if(a2[1].length !== b2[1].length) return a2[0].localeCompare(b2[0]);
+		r = a2[1].localeCompare(b2[1]) || (+a2[2]-b2[2]);
+		if(r) return r;
 	}
-	for(var i = 0, j = 0; i < a.length && j < b.length; ++i, ++j) {
-		ca = a[i];
-		cb = b[j]; // TODO: Even better, just keep track of the positions and use .slice()
-		if(numeric(ca) && numeric(cb)) {
-			for(ca = [ca]; numeric(a[i + 1]); ++i) ca.push(a[i + 1]);
-			for(cb = [cb]; numeric(b[j + 1]); ++j) cb.push(b[j + 1]);
-			diff = parseInt(ca.join(""), 10) - parseInt(cb.join(""), 10);
-		} else {
-			diff = ca.localeCompare(cb);
-		}
-		if(diff) return diff;
-	}
-	return (a.length - i) - (b.length - j); // TODO: Should we be smarter about file extensions somehow?
 };
