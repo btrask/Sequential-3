8ba3c797dad3a3160916c9a5095ee9140b17192d
Ben Trask <bentrask@comcast.net>
2013-02-25 07:46:47 -0500

    Declare more JSHint globals.

--- client/gallery/index.js
+++ client/gallery/index.js
@@ -20,7 +20,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-/*global Index*/
+/*global Index config DOM bt*/
 if(!config.windowState) config.windowState = function() {
 	var state = {};
 	state.path = window.location.pathname || "/";
